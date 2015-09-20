e4e7b3f2491171d3de429362f848304fb53466f8
Phil Johnston <support@mintplugins.com>
2015-09-20 19:03:09 -0400

    Move to Mint

--- includes/updater/mp-stacks-second-video-update.php
+++ includes/updater/mp-stacks-second-video-update.php
@@ -7,7 +7,7 @@
  * @package    MP Stacks Second Video
  * @subpackage Functions
  *
- * @copyright  Copyright (c) 2013, Move Plugins
+ * @copyright  Copyright (c) 2014, Mint Plugins
  * @license    http://opensource.org/licenses/gpl-2.0.php GNU Public License
  * @author     Philip Johnston
  */
@@ -23,7 +23,7 @@
 	function mp_stacks_second_video_update() {
 		$args = array(
 			'software_name' => 'MP Stacks + Second Video', //<- The exact name of this Plugin.
-			'software_api_url' => 'http://moveplugins.com',//The URL where Second Video and mp_repo are installed and checked
+			'software_api_url' => 'http://mintplugins.com',//The URL where Second Video and mp_repo are installed and checked
 			'software_filename' => 'mp-stacks-second-video.php',
 			'software_licensed' => false, //<-Boolean
 		);
